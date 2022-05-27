1baa07686a47551a00e08edfe1e3584d07f1c48e
Daniel Godines <danielgodinez123@yahoo.com>
2022-05-27 12:49:52 -0600

    Update analysis.py

--- StreamingInstability_YJ14/analysis.py
+++ StreamingInstability_YJ14/analysis.py
@@ -10,143 +10,52 @@ import matplotlib.pyplot as plt
 import numpy as np
 import astropy.constants as const
 warnings.filterwarnings("ignore")
+from pathlib import Path
+import pkg_resources
 
-rhop = np.load("/Users/daniel/Desktop/density_cube.npy")
+from radiative_transport import
+
+def load_cube():
+    """
+    Loads 256 x 256 x 256 density cube. Corresponds to one snapshot of a 100
+    period simulation of the streaming instability in a protoplanetary disk,
+    provided by Yang & Johansen (2014).
+
+    Returns:
+        The first output is the 3D array of the cube, the second output
+        is the axis array. This array can be used as either axis (z,y,x).
+    """
+    resource_package = __name__
+    resource_path = '/'.join(('data', 'density_cube_1.npy'))
+    file = pkg_resources.resource_filename(resource_package, resource_path)
+    density_cube_1 = np.load(file)
+
+    resource_path = '/'.join(('data', 'density_cube_2.npy'))
+    file = pkg_resources.resource_filename(resource_package, resource_path)
+    density_cube_2 = np.load(file)
+
+    resource_path = '/'.join(('data', 'axis'))
+    file = pkg_resources.resource_filename(resource_package, resource_path)
+    axis = np.loadtxt(file)
+
+    return np.r_[density_cube_1, density_cube_2], axis
+
 
 h,k,c,sb,au= 6.626e-27, 1.3807e-16, 2.997e10, 5.67e-5,  const.au.cgs.value
-
-#r = 50 * au #position of shearing box
-#h = 0.1 #scale ratio
-#H = h * r #scale height (5 AU)
-
-#x, y, z = f.x, f.y, f.z
-#xp, yp, zp = fp.xp, fp.yp, fp.zp
-
-#dust_to_gas = 1e-2
-#sigma0 = 1000 #column density at 1 AU
-#sigma = sigma0 * (r / au) ** (-1.)
-#rho = sigma / H / np.sqrt(2*np.pi) #multiply to density to make unitless
-
 T = 30
 kappa_0 = 2e-4
 kappa = kappa_0 * T**2.1 #Opacity is function of T at low temperatures (See Table 2: https://iopscience.iop.org/article/10.1086/304514/pdf) & Figure 1 (https://arxiv.org/pdf/astro-ph/0308344.pdf)
 
 
-def blackbody(T, nu):
-    """
-    Planck's law, which describes the black body radiation 
-    of a source in thermal equilibrium at a given temperature T.
-
-    Args:
-        T (float): Temperature of the source.
-        nu (ndarray): Array of frequencies.
-
-    Returns:
-        Spectral radiance of the source. 
-    """
-    bb = 2*h*nu**3 / (c**2*(np.exp(h*nu / (k*T)) - 1))
-
-    return bb
-
-def calculate_tau(density, axis, kappa, sigma):
-    """
-    Integrates density cube along the first axis
-    of the array. This function assumes a 3D cube with 
-    a uniform grid.
-    
-    Args:
-        density (ndarry): 3D array of densities.
-        axis (ndarray): 1D array of the axis along which to integrate.
-        kappa (float): Dust opacity coefficient.
-        sigma (float): Column density of the dust
-        
-    Returns:
-        2D array containing the integrated values along the third axis.
-    """
-    
-    dz = np.diff(axis)[0]
-    nx, ny = len(axis), len(axis)
-    
-    tau = np.zeros([ny, nx]) #python reverses order
-           
-    for i in range(nx):
-        for j in range(ny):
-            surface_density = np.trapz(density[:,j,i]) * dz * sigma
-            tau[j, i] = surface_density * kappa
-        
-    return tau
-
-def calculate_t(density, axis, kappa, sigma):
-    """
-    Optical depth with respect to z position along the column.
-    Integrates from z to L. This is the optical depth as we move
-    up the column, where as the optical_depth() function calculates
-    along the entire column. 
-    
-    This function assumes a 3D cube with uniform grid.
-    
-    Args:
-        density (ndarry): 3D array of densities.
-        axis (ndarray): 1D array of the axis along which to integrate.
-        kappa (float): Dust opacity coefficient 
-        
-    Returns:
-        1D array containing the integrated values along the third axis.
-    
-    """
-    
-    dz = np.diff(axis)[0]
-    nx, ny, nz = dim.nx, dim.ny, dim.nz
-    
-    t = np.zeros(nz)
-    
-    surface_density = 0
-    for k in range(nz):        
-        surface_density += density[k] * dz * sigma
-        t[k] = surface_density * kappa
-        
-    return t   
-
-def calculate_flux(density, tau, axis, T, kappa, sigma):
-    """
-    Calculate flux using sln for RT eqn (5.113)
-    
-    Args:
-        density (ndarry): 3D array of densities.
-        tau (ndarray): 2D array of optical depth.
-        axis (ndarray): 1D array of the axis along which to integrate.
-        T (float): Temperature.
-        kappa (float): Dust opacity coefficient 
-        
-    Returns:
-        2D array containing the integrated values along the third axis.
-    """
-    
-    dx = np.diff(axis)[0]
-    nx, ny, nz = dim.nx, dim.ny, dim.nz
-    flux = np.zeros([ny, nx])
-    
-    for i in range(nx):
-        print(i)
-        for j in range(ny):
-            bb = np.zeros(nz)
-            rhod = density[:,j,i] #1D array
-            t = calculate_t(rhod, axis=axis, kappa=kappa, sigma=sigma)
-            index = np.where(rhod > 0)[0]
-            bb[index] = sb*T**4
-            flux[j, i] = np.trapz(bb*np.exp(-(tau[j,i]-t)), x=axis, dx=dx)
-    
-    return flux
-
-
+rhop, axis = load_cube()
 
 # Calculate optical depth
 n = len(z) 
 nu = np.logspace(11,15,n)
 
-tau = calculate_tau(bbb, axis=z, kappa=kappa, sigma=sigma)
+tau = calculate_tau(rhop, axis=axis, kappa=kappa, sigma=1000)
 
-plt.contourf(x, y, np.log10(tau), np.linspace(-2,2,256))
+plt.contourf(axis, axis, np.log10(tau), np.linspace(-2,2,256))
 plt.colorbar()
 plt.xlabel('x')
 plt.ylabel('y')
@@ -156,17 +65,15 @@ plt.show()
 
 
 #Calcualte outgoing flux
-#n = len(z) 
-#nu = np.logspace(11,15,n)
-nx, ny = dim.nx, dim.ny
+nx, ny = len(axis), len(axis)
+dx, dy, dz = np.diff(axis)[0], np.diff(axis)[0], np.diff(axis)[0]
 Nw = 1./(nx*ny)
-path = '/Users/daniel/Desktop/Planetary/StreamingInstability_YJ14/txt_files/'
 
-box_mass_codeunits = np.sum(f.rhop)* f.dx * f.dy * f.dz 
+box_mass_codeunits = np.sum(rhop)* dx * dy * dz 
 
 H = 5*au 
-Lx=np.abs(x[0]-x[-1])*H #Length of the box in cm
-Ly=np.abs(y[0]-y[-1])*H #Length of the box in cm
+Lx=np.abs(axis[0]-axis[-1])*H #Length of the box in cm
+Ly=np.abs(axis[0]-axis[-1])*H #Length of the box in cm
 area = Lx*Ly
 
 src_fn = sb*T**4     #Estimate flux according to optical thickness
@@ -176,7 +83,7 @@ sigma = np.linspace(1, 400, n)
 filling_factor = np.zeros(n)
 mass_excess = np.zeros(n)
 for i in range(n):
-    tau = calculate_tau(density=f.rhop, axis=z, kappa=kappa, sigma=sigma[i])
+    tau = calculate_tau(density=rhop, axis=axis, kappa=kappa, sigma=sigma[i])
     filling_factor[i] = len(np.where(tau > 1)[0]) * Nw
     #flux = calculate_flux(density=f.rhop, tau=tau, axis=z, T=T, kappa=kappa, sigma=sigma[i])
 
@@ -212,7 +119,6 @@ plt.plot(sigma, filling_factor, 'ro-')
 plt.xlabel(r'$\Sigma_d \ (g / cm^2)$', size=20)
 plt.ylabel('Filling Factor', size=20)
 plt.show()
-
 
 print("Max flux :"+str(flux.max()))
 print("Min flux :"+str(flux.min()))
