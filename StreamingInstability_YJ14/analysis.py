43d8487315563939dfcec808becd6c132560b05f
Daniel <daniel@Daniels-MBP-5.MG8702>
2022-06-17 14:45:28 -0600

    unit_sigma now updating

--- StreamingInstability_YJ14/analysis.py
+++ StreamingInstability_YJ14/analysis.py
@@ -13,6 +13,7 @@ warnings.filterwarnings("ignore")
 from pathlib import Path
 import pkg_resources
 
+
 class density_cube:
     """
     Class for a density cube object. The class methods
@@ -27,22 +28,23 @@ class density_cube:
     Args:
         density (ndarry): 3D density cube.
         axis (ndarray): 1D array of the axis along which to integrate.
+        column_density : Column density of the gas (g / cm^2)
         T (float): Temperature of the entire box, as this model is isothermal.
             Defaults to 30.  
-        kappa (float):
-        sigma (float):
+        kappa (float): Dust opacity coefficient (cm^2 / g)
+        sigma (float): Column density of the gas 
         H (int): Scale height of the box, defaults to 5.
 
     """
     
-    def __init__(self, data=None, axis=None, T=30, kappa=1, sigma=100, H=5,
+    def __init__(self, data=None, axis=None, column_density=100, T=30, kappa=1, H=5,
         Nz=None, Ny=None, Nx=None, dz=None, dy=None, dx=None, Lx=None, Ly=None):
 
         self.data = data
         self.axis = axis 
+        self.column_density = column_density
         self.T = T 
         self.kappa = kappa
-        self.sigma = sigma
         self.H = H*const.au.cgs.value
         self.Lx = Lx 
         self.Ly = Ly
@@ -60,14 +62,16 @@ class density_cube:
             else:
                 self.data = load_cube()[0]
 
+        self.unit_sigma = self.column_density / np.sqrt(2*np.pi)
         self.tau = None 
         self.flux = None
         self.mass = None
         self.mass_excess = None
-        
+        self.filling_factor = None 
         self.Nw = None
         self.area = None 
         self.mass = None
+
         self.configure()
 
     def configure(self):
@@ -76,7 +80,7 @@ class density_cube:
         attributes. If sigma, H, or dx/dy/dz attributes are updated, re-run this method 
         to re-configure the object.
         """
-        print('Initializing parameters and attributes... By default model assumes symmetrical box, but if sigma, H, dx/dy/dz or Lx/Ly attributes are updated, run the configure() method to update the object.')
+       #print('Initializing parameters and attributes...')
         if self.Nz is None:
             self.Nz = len(self.axis)
         if self.Ny is None:
@@ -98,14 +102,15 @@ class density_cube:
         if self.Ly is None: 
             self.Ly = np.abs(self.axis[0] - self.axis[-1])*self.H 
 
+        self.unit_sigma = self.column_density / np.sqrt(2*np.pi)
         #Observed mass of the dust in the box
         self.area = self.Lx * self.Ly
             
         #Code units
-        gas_column_density = self.sigma * np.sqrt(2*np.pi) #Gas column density
         box_mass_codeunits = np.sum(self.data)* self.dx * self.dy * self.dz 
-        unit_mass = self.sigma * self.H**2
+        unit_mass = self.unit_sigma * self.H**2
         self.mass = box_mass_codeunits * unit_mass 
+        self.calc_tau()
         self.calc_flux()
         self.calc_mass_excess()
         self.calc_filling_factor()
@@ -141,7 +146,7 @@ class density_cube:
                
         for i in range(self.Nx):
             for j in range(self.Ny):
-                surface_density = np.trapz(self.data[:,j,i]) * self.dz * self.sigma
+                surface_density = np.trapz(self.data[:,j,i]) * self.dz * self.unit_sigma
                 tau[j, i] = surface_density * self.kappa
             
         self.tau = tau
@@ -165,8 +170,9 @@ class density_cube:
         
         t = np.zeros(self.Nz)
         surface_density = 0
+
         for i in range(self.Nz):        
-            surface_density += rhod[i] * self.dz * self.sigma
+            surface_density += rhod[i] * self.dz * self.unit_sigma
             t[i] = surface_density * self.kappa
             
         return t 
@@ -183,7 +189,7 @@ class density_cube:
         """
 
         if self.tau is None:
-            self.calc_tau()
+            self.calc_tau(self)
     
         flux = np.zeros([self.Ny, self.Nx])
         #Estimate flux according to optical thickness
@@ -208,7 +214,7 @@ class density_cube:
         """
 
         if self.tau is None:
-            self.calc_tau()
+            calc_tau(self)
 
         #Source function should be per frequency (1mm wavelength ~ 230GHz)
         src_fn_230 = self.blackbody(nu=230e9) 
@@ -232,19 +238,20 @@ class density_cube:
         """
 
         if self.tau is None:
-            self.calc_tau()
+            self.calc_tau(self)
 
         self.filling_factor = len(np.where(self.tau > 1)[0]) * self.Nw
 
         return 
 
+        
     def plot_tau(self):
         """
         Plots the optical depth at the exit plane.
         """
 
         if self.tau is None:
-            self.calc_tau()
+            self.calc_tau(self)
 
         plt.contourf(self.axis, self.axis, np.log10(self.tau), np.linspace(-2,2,256))
         plt.colorbar()
@@ -255,10 +262,10 @@ class density_cube:
 
     def plot_flux(self):
         """
-        Plots the outgoing at the exit plane.
+        Plots the outgoing flux at the exit plane.
         """
         if self.flux is None:
-            self.calc_flux()
+            self.calc_flux(self)
 
         plt.contourf(self.axis, self.axis, self.flux, 256)
         plt.xlabel('x')
@@ -273,24 +280,25 @@ class density_cube:
 def plot_filling_factor(filling_factor, sigma):
     """
     Plots Gas Column Density vs Filling Factor
+
+    Args:
+        sigma = column density
     """
 
-    gas_column_density = sigma * np.sqrt(2*np.pi) #Gas column density
 
-    plt.plot(gas_column_density, filling_factor, 'ro-')
+    plt.plot(sigma, filling_factor, 'ro-')
     plt.xscale('log')
     plt.xlabel(r'$\Sigma_g \ (g / cm^2)$', size=20)
     plt.ylabel('Filling Factor', size=20)
     plt.show()
 
-def plot_mass_excess(self):
+def plot_mass_excess(column_density, mass_excess):
     """
     Plots Gas Column Density vs Mass Excess
+    Column density of gas
     """
 
-    gas_column_density = sigma * np.sqrt(2*np.pi) #Gas column density
-
-    plt.plot(gas_column_density, mass_excess, 'ro-')
+    plt.plot(column_density, mass_excess, 'ro-')
     plt.xscale('log')
     plt.xlabel(r'$\Sigma_g \ (g / cm^2)$', size=20)
     plt.ylabel('Mass Excess', size=20)
@@ -321,4 +329,5 @@ def load_cube():
         data, axis = np.r_[density_cube_1, density_cube_2], np.loadtxt(file)
 
         return data, axis
+
 
